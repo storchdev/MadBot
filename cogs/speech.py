35ddf49e9197e5fa9c2cdc25caef200a0ea6bfff
Stormtorch002 <pianister123@gmail.com>
2021-07-07 11:24:50 -0400

    add help strings

--- cogs/speech.py
+++ cogs/speech.py
@@ -13,24 +13,29 @@ class PartsOfSpeech(commands.Cog, name='Parts of Speech',
 
     @commands.group(invoke_without_command=True, aliases=['partsofspeech', 'speech'])
     async def pos(self, ctx):
+        """Gives information on the main parts of speech, i.e. noun, adjective, verb, adverb."""
         text = f'''**There are 4 main parts of speech in MadLibs:**  `noun`, `adjective`, `verb`, `adverb`.
 To get more info, you can use `{ctx.prefix.lower()}{ctx.invoked_with} <part of speech>`.'''
         await ctx.send(text)
 
     @pos.command()
     async def noun(self, ctx):
+        """Gives information on what a noun is."""
         await ctx.send(self.speech['noun'])
 
     @pos.command()
     async def adverb(self, ctx):
+        """Gives information on what an adverb is."""
         await ctx.send(self.speech['adverb'])
 
     @pos.command()
     async def verb(self, ctx):
+        """Gives information on what a verb is."""
         await ctx.send(self.speech['verb'])
 
     @pos.command()
     async def adjective(self, ctx):
+        """Gives information on what an adjective is."""
         await ctx.send(self.speech['adjective'])
 
 
